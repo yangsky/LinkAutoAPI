86098b1c03093bcb9d22de15185626a983237cab
yangsky <yang8730598@126.com>
2016-10-29 23:09:46 +0800

    add autolink framework

---
+++ AutoLink/AutoLink/AutoLink/AutoLink.h
@@ -0,0 +1,19 @@
+//
+//  AutoLink.h
+//  AutoLink
+//
+//  Created by Yang Shengyuan on 16/10/29.
+//  Copyright © 2016年 Yang Shengyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoLink.
+FOUNDATION_EXPORT double AutoLinkVersionNumber;
+
+//! Project version string for AutoLink.
+FOUNDATION_EXPORT const unsigned char AutoLinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLink/PublicHeader.h>
+
+
